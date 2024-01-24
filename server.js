6993d8b42cb345d3ab12df6288721d2ea943e182
Yufan <yuxu1@users.noreply.github.com>
2024-01-23 20:29:28 -0800

    add files

---
+++ server.js
@@ -0,0 +1,40 @@
+const url = require('url');
+const fs = require('fs');
+const http = require('http');
+
+http.createServer((request, response) => {
+    let addr = request.url;
+    let q = new URL(addr, 'http://localhost:8080');
+    let filePath = '';
+
+    fs.appendFile(
+      'log.txt',
+      'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n',
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Added to log.');
+        }
+      }
+    );
+
+    if (q.pathname.includes('documentation')) {
+      filePath = __dirname + '/documentation.html';
+    } else {
+      filePath = 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+    });
+    response.write(data);
+    response.end();
+  })
+  .listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
